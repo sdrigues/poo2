b21e31b1be2c712109eea6db361256547e07e8db
sdrigues <rodrigues.sam97@gmail.com>
2020-03-10 21:42:38 -0300

    first Commit

---
+++ src/test/java/com/example/ac1poo2/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ac1poo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
